9786ffea04ebd8c19e7d5181b2704f56ee89fd92
Kevin Osorio <63277793+k3v1n0s0r10@users.noreply.github.com>
2023-02-20 07:25:07 -0500

    Update user_profile.ps1

--- user_profile.ps1
+++ user_profile.ps1
@@ -2,9 +2,8 @@
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
 Import-Module posh-git
-Import-Module oh-my-posh
-$omp_config = Join-Path $PSScriptRoot ".\takuya.omp.json"
-oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+
+oh-my-posh init pwsh --config ~/.takuya.omp.json | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
 
@@ -31,4 +30,3 @@ function which ($command) {
   Get-Command -Name $command -ErrorAction SilentlyContinue |
     Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
-
